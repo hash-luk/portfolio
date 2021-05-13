8bdca19202a88f178132072eba9979d5afc310e0
hash-luke <lucas.patrick2506@gmail.com>
2021-05-13 15:40:24 -0300

    Styles and Functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,26 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'portfolio';
+
+  redirectDevFinance() {
+    document.location.href='https://github.com/hash-luk/Dev-Finance';
+  }
+
+  redirectMoveIt() {
+    document.location.href='https://github.com/hash-luk/Move.it';
+  }
+
+  redirectHappy() {
+    document.location.href='https://github.com/hash-luk/Happy-NLW-3';
+  }
+
+  redirectSpiderMan() {
+    document.location.href='https://github.com/hash-luk/SpiderMan-Home-Page';
+  }
+
+  redirectPinterestClone() {
+    document.location.href='https://github.com/hash-luk/pinterest-ui-clone';
+  }
 }
