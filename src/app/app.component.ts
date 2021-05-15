1992cec8396e4f9d212d9e45b924842b802b222b
hash-luke <lucas.patrick2506@gmail.com>
2021-05-15 14:19:34 -0300

    Starting Dev Email

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Form,Validators,FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,23 +9,55 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
 
-  redirectDevFinance() {
-    document.location.href='https://github.com/hash-luk/Dev-Finance';
+  public form :FormGroup;
+
+
+  constructor (private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name:['',Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(15),
+        Validators.required
+      ])],
+      about:['',Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(20),
+        Validators.required
+      ])],
+      message:['',Validators.compose([
+        Validators.minLength(10),
+        Validators.maxLength(300),
+        Validators.required
+      ])]
+    })
   }
 
-  redirectMoveIt() {
-    document.location.href='https://github.com/hash-luk/Move.it';
+
+
+  redirect = {
+
+    redirectDevFinance() {
+      document.location.href='https://github.com/hash-luk/Dev-Finance';
+    },
+
+    redirectMoveIt() {
+      document.location.href='https://github.com/hash-luk/Move.it';
+    },
+
+    redirectHappy() {
+      document.location.href='https://github.com/hash-luk/Happy-NLW-3';
+    },
+
+    redirectSpiderMan() {
+      document.location.href='https://github.com/hash-luk/SpiderMan-Home-Page';
+    },
+
+    redirectPinterestClone() {
+      document.location.href='https://github.com/hash-luk/pinterest-ui-clone';
+    }
   }
 
-  redirectHappy() {
-    document.location.href='https://github.com/hash-luk/Happy-NLW-3';
-  }
 
-  redirectSpiderMan() {
-    document.location.href='https://github.com/hash-luk/SpiderMan-Home-Page';
-  }
 
-  redirectPinterestClone() {
-    document.location.href='https://github.com/hash-luk/pinterest-ui-clone';
-  }
+
 }
